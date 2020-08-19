7a8b6a911538a9d389f357b4e4add19cafe1ae3b
pablow33cao <pablitoariasortega@gmail.com>
2020-08-19 17:48:56 -0400

    primer parcial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { ProductoComponent } from './producto/producto.component';
 import {FormsModule} from '@angular/forms';
+import { TallaPipe } from './talla.pipe';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    ProductoComponent,
+    TallaPipe
     
   ],
   imports: [
