5db3d4b4547b9aa8275e37d9f9d657ddcbe4cfd0
pablow33cao <pablitoariasortega@gmail.com>
2020-08-19 15:08:03 -0400

    ExamenBase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {Test2Component} from './test2/test2.component';
-import {TestComponent} from './components/test.component';
+
 import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    
   ],
   imports: [
     BrowserModule,
