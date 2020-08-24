144d20d1ca4b423aa853f42f7f40db66e6303385
unknown <pablitoariasortega@gmai.com>
2020-08-24 14:59:37 -0400

    navegation for modules

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user/user.component';
+import { User1Component } from './user1/user1.component';
+import { User2Component } from './user2/user2.component';
 
 
 @NgModule({
-  declarations: [UserComponent],
+  declarations: [UserComponent, User1Component, User2Component],
   imports: [
     CommonModule,
     UserRoutingModule
