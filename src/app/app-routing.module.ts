28061aca35c18b53b65b5aa70557d53930640c51
unknown <pablitoariasortega@gmai.com>
2020-09-06 00:51:58 -0400

    architecture login and pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path:'',redirectTo:'home',pathMatch:'full'},
-  {path:'home',loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
-  {path:'admin',loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule)},
-  {path:'list',loadChildren: () => import('./pages/list/list.module').then(m => m.ListModule)},
+
+  {path:'',redirectTo:'login',pathMatch:'full'},
+  {path:'login',loadChildren: () => import('./login/login.module').then(m => m.LoginModule)},
+  {path:'pages',loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)},
+
+  // {path:'',redirectTo:'home',pathMatch:'full'},
+  // {path:'home',loadChildren: () => import('./pages1/home/home.module').then(m => m.HomeModule)},
+  // {path:'admin',loadChildren: () => import('./pages1/admin/admin.module').then(m => m.AdminModule)},
+  // {path:'list',loadChildren: () => import('./pages1/list/list.module').then(m => m.ListModule)},
 
 ];
 
