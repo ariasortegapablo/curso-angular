1696466c4c1730bd1de6de39ed756bcd67443523
unknown <pablitoariasortega@gmai.com>
2020-09-06 14:55:30 -0400

    login with template-driven form

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login/login.component';
 import {NgElseDirective} from '../directives/ng-else.directive';
+import {FormsModule} from '@angular/forms';
+import {NgModule} from '@angular/core';
 
 
 @NgModule({
@@ -13,7 +15,8 @@ import {NgElseDirective} from '../directives/ng-else.directive';
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    FormsModule
   ]
 })
 export class LoginModule { }
